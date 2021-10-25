e8cc7c138f31d767842c8a812b31287a367891eb
Matheus Rian <66932079+matheusrian36@users.noreply.github.com>
2021-10-25 14:59:48 -0300

    Update try e datasource
    Uso do try with resources, começo da utilização do pool de conexões e de datasource

--- src/main/java/TestaInsercaoComParametro.java
+++ src/main/java/TestaInsercaoComParametro.java
@@ -3,25 +3,21 @@ import java.sql.*;
 public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
                ConnectionFactory connectionFactory = new ConnectionFactory();
-        Connection connection = connectionFactory.recuperarConexao();
-        connection.setAutoCommit(false);
+        try(Connection connection = connectionFactory.recuperarConexao()) {
+            connection.setAutoCommit(false);
 
-        try{
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
-            adicionarVariavel("SmartTV", "45 polegadas", statement);
-            adicionarVariavel("Radio", "Radio de bateria", statement);
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)){
+                adicionarVariavel("SmartTV", "45 polegadas", statement);
+                adicionarVariavel("Radio", "Radio de bateria", statement);
 
-            connection.commit();
-
-            statement.close();
-            connection.close();
-        }catch (Exception e){
-            e.printStackTrace();
-            System.out.println("ROLLBACK EXECUTADO");
-            connection.rollback();
+                connection.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("ROLLBACK EXECUTADO");
+                connection.rollback();
+            }
         }
-
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement statement) throws SQLException {
@@ -35,11 +31,12 @@ public class TestaInsercaoComParametro {
 
         statement.execute();
 
-        ResultSet resultSet = statement.getGeneratedKeys();
+        try(ResultSet resultSet = statement.getGeneratedKeys()) {
 
-        while(resultSet.next()){
-            Integer id = resultSet.getInt(1);
-            System.out.println("O id criado é: " + id);
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt(1);
+                System.out.println("O id criado é: " + id);
+            }
         }
     }
 }
